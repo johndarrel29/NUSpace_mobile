55ebbd880e1f814e4144801852e972c168062e2f
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2025-08-04 16:23:11 +0800

    Version 1.0.2
    New features
    1. http package
    2. .env
    3. config.dart
    4. notification_service
    - to get the FCM token
    
    pending logic
    1. push notification, (in notification_service)
    2. sending of token,
    3. fixing pattern flow (login muna bago mag send ng token sa backend)
    
    to add
    1. Login feature
    2. register feature

--- nuspace_app/lib/main.dart
+++ nuspace_app/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:nuspace_app/firebase_options.dart';
+import 'package:nuspace_app/services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseMessaging.onBackgroundMessage(
+    NotificationService.firebaseBackgroundHandler,
+  );
+
   runApp(const MyApp());
 }
 
@@ -14,15 +23,31 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'NU Space',
-      // theme: ThemeData(
-      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      // ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('NU Space')),
-        body: const Center(child: Text('Firebase Initialized')),
-      ),
+    return MaterialApp(title: 'NU Space', home: const HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    NotificationService.requestPermission();
+    NotificationService.initializeFCMListerners();
+    NotificationService.getAndPrintFCMToken(
+      userId: '688c8bcc47a6783fa2509b6d',
+      role: 'student',
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text("NU Space Home")));
   }
 }
---
+++ nuspace_app/lib/services/notification_service.dart
@@ -0,0 +1,87 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:nuspace_app/config/config.dart';
+import 'package:nuspace_app/firebase_options.dart';
+import 'package:http/http.dart' as http;
+
+class NotificationService {
+  static final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+
+  //call this in main() to handle background messages
+  static Future<void> firebaseBackgroundHandler(RemoteMessage message) async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    print('Background notification: ${message.notification?.title}');
+  }
+
+  //call this in inisState()
+  static Future<void> requestPermission() async {
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+    print('Permission status: ${settings.authorizationStatus}');
+  }
+
+  static void initializeFCMListerners() {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Foreground notification: ${message.notification?.title}');
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('App opened from notification: ${message.notification?.title}');
+    });
+  }
+
+  static Future<void> getAndPrintFCMToken({
+    required String userId,
+    required String role,
+  }) async {
+    String? token = await _messaging.getToken();
+    print('FCM Token: $token');
+
+    if (token != null) {
+      await _sendTokenToBackend(token, userId, role);
+    }
+  }
+
+  //send token to backend
+  static Future<void> _sendTokenToBackend(
+    String token,
+    String userId,
+    String role,
+  ) async {
+    try {
+      final String platform = Platform.isAndroid ? 'android' : 'ios';
+
+      final response = await http.post(
+        Uri.parse('${AppConfig.baseUrl}/api/device-token'),
+        headers: {
+          'Content-Type': 'application/json',
+          //Authorization JWT Token
+        },
+        body: jsonEncode({
+          'userId': userId,
+          'role': role,
+          'token': token,
+          'platform': platform,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        print('Token sent to backend');
+      } else {
+        print("Failed to send token: ${response.statusCode}");
+        print('Response: ${response.body}');
+      }
+    } catch (e, stackTrace) {
+      print("Error in sending token to backend: $e");
+      print("stacktrace: $stackTrace");
+    }
+  }
+}
